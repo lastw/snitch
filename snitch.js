1434ff67298cfb4572e7e6253dc9413205390aa4
Dmitry unknownexception <unknownliveid@hotmail.com>
2014-05-13 16:18:30 +0700

    handle circural structures more properly

--- snitch.js
+++ snitch.js
@@ -1,199 +1,209 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  // Date.now() polifyll for IE8-
-  if (!Date.now) {
-    Date.now = function now() {
-      return new Date().getTime();
-    };
-  }
+	// Date.now() polifyll for IE8-
+	if (!Date.now) {
+		Date.now = function now() {
+			return new Date().getTime();
+		};
+	}
 
-  var Snitch = function (options) {
-    options = options || {};
+	var Snitch = function (options) {
+		options = options || {};
 
-    if (typeof options === 'string') {
-      options = {
-        url: options
-      };
-    }
-    else {
-      options.url = options.url || location.href;
-    }
+		if (typeof options === 'string') {
+			options = {
+				url: options
+			};
+		} else {
+			options.url = options.url || location.href;
+		}
 
-    this.KEY = options.key || 'snitch';
-    this.url = options.url;
-    this.ttl = options.ttl;
-    this.interval = options.interval;
-    this.capacity = options.capacity;
-    if (!this.ttl) {
-      this.checkTTL = function () {};
-    }
-    if (!this.capacity) {
-      this.checkCapacity = function () {};
-    }
-    this._log = [];
-    this.storage = Snitch.storage;
-    this.load();
+		this.KEY = options.key || 'snitch';
+		this.url = options.url;
+		this.ttl = options.ttl;
+		this.interval = options.interval;
+		this.capacity = options.capacity;
+		if (!this.ttl) {
+			this.checkTTL = function () {};
+		}
+		if (!this.capacity) {
+			this.checkCapacity = function () {};
+		}
+		this._log = [];
+		this.storage = Snitch.storage;
+		this.load();
 
-    if (this.interval) {
-      var firstTimestamp = this._log[0] ? this._log[0][0] : Date.now(),
-        timeout = firstTimestamp - Date.now() + this.interval,
-        _this = this;
-      setTimeout(function () {
-        _this.send();
-        setInterval(_this.send.bind(_this), _this.interval);
-      }, timeout);
-    }
+		if (this.interval) {
+			var firstTimestamp = this._log[0] ? this._log[0][0] : Date.now(),
+				timeout = firstTimestamp - Date.now() + this.interval,
+				_this = this;
+			setTimeout(function () {
+				_this.send();
+				setInterval(_this.send.bind(_this), _this.interval);
+			}, timeout);
+		}
 
-  };
+	};
 
-  Snitch.storage = {
-    get: function (key) {
-      return Snitch.parse(localStorage.getItem(key));
-    },
+	Snitch.storage = {
+		get: function (key) {
+			return Snitch.parse(localStorage.getItem(key));
+		},
 
-    set: function (key, value) {
-      return localStorage.setItem(key, Snitch.serialize(value));
-    },
+		set: function (key, value) {
+			return localStorage.setItem(key, Snitch.serialize(value));
+		},
 
-    clear: function (key) {
-      return localStorage.removeItem(key);
-    }
-  };
+		clear: function (key) {
+			return localStorage.removeItem(key);
+		}
+	};
 
-  Snitch.message = function () {
-    var message = '';
-    for (var i in arguments) {
-      var part = arguments[i];
-      if (typeof part !== 'string') {
-        part = Snitch.serialize(arguments[i]);
-      }
-      message += part + ' ';
-    }
-    // cut last space
-    return message.slice(0, -1);
-  };
+	Snitch.message = function () {
+		var message = '';
+		for (var i in arguments) {
+			var part = arguments[i];
+			if (typeof part !== 'string') {
+				part = Snitch.serialize(arguments[i]);
+			}
+			message += part + ' ';
+		}
+		// cut last space
+		return message.slice(0, -1);
+	};
 
-  Snitch.send = function (options) {
-    options.method = options.method || 'POST';
-    $.ajax(options);
-  };
+	Snitch.send = function (options) {
+		options.method = options.method || 'POST';
+		$.ajax(options);
+	};
 
-  Snitch.serialize = function (struct) {
-    var result;
-    try {
-      result = JSON.stringify(struct);
-    }
-    catch(e) {
-      result = 'snitch serialization error: ' + e.message;
-    }
-    return result;
-  };
+	Snitch.serialize = function (struct) {
+		var result;
+		try {
+			result = JSON.stringify(struct);
+		} catch (e) {
+			var cache = [];
+			result = JSON.stringify(struct, function (key, value) {
+				if (typeof value === 'object' && value !== null) {
+					if (cache.indexOf(value) !== -1) {
+						// Circular reference found, discard key
+						return;
+					}
+					// Store value in our collection
+					cache.push(value);
+				}
+				return value;
+			});
+			cache = null; // Enable garbage collection
+		}
+		return result;
+	};
 
-  Snitch.parse = function (str) {
-    var result;
-    try {
-      result = JSON.parse(str);
-    }
-    catch(e) {
-      result = 'parse error: ' + e.message;
-    }
-    return result;
-  };
+	Snitch.parse = function (str) {
+		var result;
+		try {
+			result = JSON.parse(str);
+		} catch (e) {
+			result = 'parse error: ' + e.message;
+		}
+		return result;
+	};
 
-  Snitch.extend = $.extend;
+	Snitch.extend = $.extend;
 
-  Snitch.filter = (typeof (_) !== 'undefined' && _.filter ? _.filter : lodashFilter);
+	Snitch.filter = (typeof (_) !== 'undefined' && _.filter ? _.filter :
+		lodashFilter);
 
-  Snitch.prototype.serialize = function () {
-    return Snitch.serialize(this._log);
-  };
+	Snitch.prototype.serialize = function () {
+		return Snitch.serialize(this._log);
+	};
 
-  Snitch.prototype.log = function () {
-    this.last = {
-      message: Snitch.message.apply(this, arguments),
-      date: Date.now()
-    };
+	Snitch.prototype.log = function () {
+		this.last = {
+			message: Snitch.message.apply(this, arguments),
+			date: Date.now()
+		};
 
-    this._log.push([
+		this._log.push([
       this.last.date,
       this.last.message
     ]);
 
-    this.checkTTL();
-    this.checkCapacity();
-    this.save();
-  };
+		this.checkTTL();
+		this.checkCapacity();
+		this.save();
+	};
 
-  Snitch.prototype.clear = function (hard) {
-    this._log = [];
-    if (hard) {
-      this.save();
-    }
-  };
+	Snitch.prototype.clear = function (hard) {
+		this._log = [];
+		if (hard) {
+			this.save();
+		}
+	};
 
-  Snitch.prototype.clearAll = function () {
-    Snitch.storage.clear(this.KEY);
-    this.clear();
-  };
+	Snitch.prototype.clearAll = function () {
+		Snitch.storage.clear(this.KEY);
+		this.clear();
+	};
 
-  Snitch.prototype.save = function () {
-    var log = this.storage.get(this.KEY);
-    log = log || {};
-    log[this.url] = this._log;
-    this.storage.set(this.KEY, log);
-  };
+	Snitch.prototype.save = function () {
+		var log = this.storage.get(this.KEY);
+		log = log || {};
+		log[this.url] = this._log;
+		this.storage.set(this.KEY, log);
+	};
 
-  Snitch.prototype.load = function () {
-    var stored = this.storage.get(this.KEY) || {};
-    this._log = stored[this.url] || [];
-  };
+	Snitch.prototype.load = function () {
+		var stored = this.storage.get(this.KEY) || {};
+		this._log = stored[this.url] || [];
+	};
 
-  Snitch.prototype.send = function (options) {
-    if (this._log.length) {
-      options = Snitch.extend({
-        url: this.url,
-        data: {
-          userAgent: navigator.userAgent,
-          log: this.serialize()
-        },
-        complete: this.clear.bind(this, true) // log will be cleared even if ajax error
-      }, options);
-      Snitch.send(options);
-    }
-  };
+	Snitch.prototype.send = function (options) {
+		if (this._log.length) {
+			options = Snitch.extend({
+				url: this.url,
+				data: {
+					userAgent: navigator.userAgent,
+					log: this.serialize()
+				},
+				complete: this.clear.bind(this, true) // log will be cleared even if ajax error
+			}, options);
+			Snitch.send(options);
+		}
+	};
 
-  Snitch.prototype.checkTTL = function () {
-    var deadline = Date.now() - this.ttl;
-    if (this._log[0][0] < deadline) {
-      this._log = Snitch.filter(this._log, function (val) {
-        return val[0] >= deadline;
-      });
-    }
-  };
+	Snitch.prototype.checkTTL = function () {
+		var deadline = Date.now() - this.ttl;
+		if (this._log[0][0] < deadline) {
+			this._log = Snitch.filter(this._log, function (val) {
+				return val[0] >= deadline;
+			});
+		}
+	};
 
-  Snitch.prototype.checkCapacity = function () {
-    this._log = this._log.slice(-this.capacity);
-  };
+	Snitch.prototype.checkCapacity = function () {
+		this._log = this._log.slice(-this.capacity);
+	};
 
-  function lodashFilter(collection, condition) {
-    var result = [],
-        index = -1,
-        length = collection ? collection.length : 0;
+	function lodashFilter(collection, condition) {
+		var result = [],
+			index = -1,
+			length = collection ? collection.length : 0;
 
-    if (typeof length === 'number') {
-      while (++index < length) {
-        var value = collection[index];
-        if (condition(value, index, collection)) {
-          result.push(value);
-        }
-      }
-    }
-    return result;
-  }
+		if (typeof length === 'number') {
+			while (++index < length) {
+				var value = collection[index];
+				if (condition(value, index, collection)) {
+					result.push(value);
+				}
+			}
+		}
+		return result;
+	}
 
-  /** Used as a reference to the global object */
-  var root = window || this;
+	/** Used as a reference to the global object */
+	var root = window || this;
 
-  root.Snitch = Snitch; // TODO: more suitable for diffirent modules systems and builders
+	root.Snitch = Snitch; // TODO: more suitable for diffirent modules systems and builders
 }.call(this));
--- tests/main.js
+++ tests/main.js
@@ -3,185 +3,195 @@
 var expect = chai.expect;
 
 describe('common', function () {
-  var snitch = new Snitch();
+	var snitch = new Snitch();
 
-  it('snitch.clear() should clear log array', function () {
-    snitch.clear();
-    expect(snitch.serialize()).to.equal('[]');
-  });
+	it('snitch.clear() should clear log array', function () {
+		snitch.clear();
+		expect(snitch.serialize()).to.equal('[]');
+	});
 
-  it('snitch.log() increases log array length by one', function () {
-    snitch.clear();
-    snitch.log('First test log message');
-    expect(snitch._log.length).to.equal(1);
-    snitch.log('Second test log message');
-    expect(snitch._log.length).to.equal(2);
-  });
+	it('snitch.log() increases log array length by one', function () {
+		snitch.clear();
+		snitch.log('First test log message');
+		expect(snitch._log.length).to.equal(1);
+		snitch.log('Second test log message');
+		expect(snitch._log.length).to.equal(2);
+	});
 
-  it('pass url to constuctor as string variable', function () {
-    var url = '/path/to/log/service',
-        snitch = new Snitch(url);
-    expect(snitch.url).to.equal(url);
-  });
+	it('pass url to constuctor as string variable', function () {
+		var url = '/path/to/log/service',
+			snitch = new Snitch(url);
+		expect(snitch.url).to.equal(url);
+	});
 
-  it('pass url to constuctor as options.url', function () {
-    var url = '/path/to/log/service',
-        snitch = new Snitch({url: url});
-    expect(snitch.url).to.equal(url);
-  });
+	it('pass url to constuctor as options.url', function () {
+		var url = '/path/to/log/service',
+			snitch = new Snitch({
+				url: url
+			});
+		expect(snitch.url).to.equal(url);
+	});
 });
 
 describe('logging', function () {
-  var snitch = new Snitch();
+	var snitch = new Snitch();
 
-  it('multiple strings logging', function () {
-    snitch.clear();
-    snitch.log('string1', 'string2', 'string3');
-    expect(snitch.last.message).to.equal('string1 string2 string3');
-  });
+	it('multiple strings logging', function () {
+		snitch.clear();
+		snitch.log('string1', 'string2', 'string3');
+		expect(snitch.last.message).to.equal('string1 string2 string3');
+	});
 
-  it('strings and numbers logging', function () {
-    snitch.clear();
-    snitch.log(1, 'string', 2);
-    expect(snitch.last.message).to.equal('1 string 2');
-  });
+	it('strings and numbers logging', function () {
+		snitch.clear();
+		snitch.log(1, 'string', 2);
+		expect(snitch.last.message).to.equal('1 string 2');
+	});
 
-  it('objects logging', function () {
-    snitch.clear();
-    snitch.log({x: 1});
-    expect(snitch.last.message).to.equal('{"x":1}');
-  });
+	it('objects logging', function () {
+		snitch.clear();
+		snitch.log({
+			x: 1
+		});
+		expect(snitch.last.message).to.equal('{"x":1}');
+	});
 
-  it('objects and numbers logging', function () {
-    snitch.clear();
-    snitch.log({x: 1}, 1);
-    expect(snitch.last.message).to.equal('{"x":1} 1');
-  });
+	it('objects and numbers logging', function () {
+		snitch.clear();
+		snitch.log({
+			x: 1
+		}, 1);
+		expect(snitch.last.message).to.equal('{"x":1} 1');
+	});
 });
 
 describe('storage', function () {
-  var snitch = new Snitch();
+	var snitch = new Snitch();
 
-  it('log is the same after save/load', function () {
-    snitch.clear();
-    snitch.log('First test log message');
-    snitch.log('Second test log message');
+	it('log is the same after save/load', function () {
+		snitch.clear();
+		snitch.log('First test log message');
+		snitch.log('Second test log message');
 
-    var snitch2 = new Snitch();
-    expect(snitch2.serialize()).to.equal(snitch.serialize());
-  });
+		var snitch2 = new Snitch();
+		expect(snitch2.serialize()).to.equal(snitch.serialize());
+	});
 
-  it('log is the same after save/load (with options)', function () {
-    snitch.clear();
-    snitch.log('First test log message');
-    snitch.log('Second test log message');
+	it('log is the same after save/load (with options)', function () {
+		snitch.clear();
+		snitch.log('First test log message');
+		snitch.log('Second test log message');
 
-    var snitch2 = new Snitch({customOption: 1});
-    expect(snitch2.serialize()).to.equal(snitch.serialize());
-  });
+		var snitch2 = new Snitch({
+			customOption: 1
+		});
+		expect(snitch2.serialize()).to.equal(snitch.serialize());
+	});
 
-  it('storages with different urls not affect each other', function () {
-    snitch.clear();
-    var snitch2 = new Snitch('/path/to/log/service');
-    snitch2.clear();
-    snitch2.save();
+	it('storages with different urls not affect each other', function () {
+		snitch.clear();
+		var snitch2 = new Snitch('/path/to/log/service');
+		snitch2.clear();
+		snitch2.save();
 
-    snitch.log('Test message');
-    snitch2.load();
-    expect(snitch2._log.length).to.equal(0);
-  });
+		snitch.log('Test message');
+		snitch2.load();
+		expect(snitch2._log.length).to.equal(0);
+	});
 
-  it('hard clearing clears storage', function () {
-    snitch.clear();
-    snitch.log('Test message');
-    snitch.clear();
-    snitch.load();
-    expect(snitch._log.length).to.equal(1);
-    snitch.clear(true);
-    snitch.load();
-    expect(snitch._log.length).to.equal(0);
-  });
+	it('hard clearing clears storage', function () {
+		snitch.clear();
+		snitch.log('Test message');
+		snitch.clear();
+		snitch.load();
+		expect(snitch._log.length).to.equal(1);
+		snitch.clear(true);
+		snitch.load();
+		expect(snitch._log.length).to.equal(0);
+	});
 });
 
 describe('log limits', function () {
-  it('remove logs with expired TTL', function (done) {
-    var snitch = new Snitch({
-      ttl: 200
-    });
-    snitch.clear();
-    snitch.log('Test message 1');
-    snitch.log('Test message 2');
-    expect(snitch._log.length).to.equal(2);
-    setTimeout(function () {
-      snitch.log('Test message 3');
-      expect(snitch._log.length).to.equal(1);
-      done();
-    }, 400);
-  });
+	it('remove logs with expired TTL', function (done) {
+		var snitch = new Snitch({
+			ttl: 200
+		});
+		snitch.clear();
+		snitch.log('Test message 1');
+		snitch.log('Test message 2');
+		expect(snitch._log.length).to.equal(2);
+		setTimeout(function () {
+			snitch.log('Test message 3');
+			expect(snitch._log.length).to.equal(1);
+			done();
+		}, 400);
+	});
 
-  it('remove logs over capacity', function () {
-    var snitch = new Snitch({
-      capacity: 3
-    });
-    snitch.clear();
-    snitch.log('Test message 1');
-    snitch.log('Test message 2');
-    snitch.log('Test message 3');
-    snitch.log('Test message 4');
-    expect(snitch._log.length).to.equal(3);
-    expect(snitch._log[0][1]).to.equal('Test message 2');
-  });
+	it('remove logs over capacity', function () {
+		var snitch = new Snitch({
+			capacity: 3
+		});
+		snitch.clear();
+		snitch.log('Test message 1');
+		snitch.log('Test message 2');
+		snitch.log('Test message 3');
+		snitch.log('Test message 4');
+		expect(snitch._log.length).to.equal(3);
+		expect(snitch._log[0][1]).to.equal('Test message 2');
+	});
 });
 
 describe('log timer', function () {
-  it('log sending every interval', function (done) {
-    var snitch = new Snitch({
-      interval: 300
-    });
-    snitch.clear();
-    snitch.log('Test interval');
-    expect(snitch._log.length).to.equal(1);
-    expect(snitch._log[0][1]).to.equal('Test interval');
-    setTimeout(function () {
-      expect(snitch._log.length).to.equal(0);
-      snitch.log('Test interval 2');
-      expect(snitch._log.length).to.equal(1);
-      expect(snitch._log[0][1]).to.equal('Test interval 2');
-      setTimeout(function () {
-        expect(snitch._log.length).to.equal(0);
-        done();
-      }, 400);
-    }, 400);
-  });
+	it('log sending every interval', function (done) {
+		var snitch = new Snitch({
+			interval: 300
+		});
+		snitch.clear();
+		snitch.log('Test interval');
+		expect(snitch._log.length).to.equal(1);
+		expect(snitch._log[0][1]).to.equal('Test interval');
+		setTimeout(function () {
+			expect(snitch._log.length).to.equal(0);
+			snitch.log('Test interval 2');
+			expect(snitch._log.length).to.equal(1);
+			expect(snitch._log[0][1]).to.equal('Test interval 2');
+			setTimeout(function () {
+				expect(snitch._log.length).to.equal(0);
+				done();
+			}, 400);
+		}, 400);
+	});
 
-  it('log sending timer offset', function (done) {
-    var snitch = new Snitch();
-    snitch.clear();
-    snitch.log('Test interval offset');
-    setTimeout(function () { // TODO: use async
-      var snitch2 = new Snitch({
-        interval: 800
-      });
-      expect(snitch2._log.length).to.equal(1);
-      expect(snitch2._log[0][1]).to.equal('Test interval offset');
-      setTimeout(function () {
-        expect(snitch2._log.length).to.equal(1);
-        setTimeout(function () {
-          expect(snitch2._log.length).to.equal(0);
-          done();
-        }, 300);
-      }, 200);
-    }, 400);
-  });
+	it('log sending timer offset', function (done) {
+		var snitch = new Snitch();
+		snitch.clear();
+		snitch.log('Test interval offset');
+		setTimeout(function () { // TODO: use async
+			var snitch2 = new Snitch({
+				interval: 800
+			});
+			expect(snitch2._log.length).to.equal(1);
+			expect(snitch2._log[0][1]).to.equal('Test interval offset');
+			setTimeout(function () {
+				expect(snitch2._log.length).to.equal(1);
+				setTimeout(function () {
+					expect(snitch2._log.length).to.equal(0);
+					done();
+				}, 300);
+			}, 200);
+		}, 400);
+	});
 });
 
 describe('errors handling', function () {
-  var snitch = new Snitch();
-  it('circular structures serialization error catch', function () {
-    snitch.clear();
-    var a = {};
-    a.b = a;
-    snitch.log(a);
-    expect(snitch._log[0][1]).to.have.string('snitch serialization error');
-  });
+	var snitch = new Snitch();
+	it('should be able to process circular structures', function () {
+		snitch.clear();
+		var a = {};
+		a.c = "test data";
+		a.b = a;
+		snitch.log(a);
+		console.log(snitch._log);
+		expect(snitch._log[0][1]).to.have.string('test data');
+	});
 });
